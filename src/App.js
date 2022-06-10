261674f40240f54519475d7984b183d86b79af14
Priyanka Patel <priyanka.empiric@gmail.com>
2022-06-10 14:28:37 +0530

    project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>Todo Dapp</h1>
+        <div>
+          <input type="text"></input>
+          <button>+</button>
+        </div>
+        <div>
+          <div className='listItem'>
+            <li>home</li>
+            <button className='listButton'>X</button>
+          </div>
+          <div className='listItem'>
+            <li>home</li>
+            <button className='listButton'>X</button>
+          </div>
+          <div className='listItem'>
+            <li>home</li>
+            <button className='listButton'>X</button>
+          </div>
+          <div className='listItem'>
+            <li>home</li>
+            <button className='listButton'>X</button>
+          </div>
+          <div className='listItem'>
+            <li>home</li>
+            <button className='listButton'>X</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
